434858afe9d6998894bb13ed404f38402ec90e90
Yomiku0617 <110821146+Yomiku0617@users.noreply.github.com>
2022-08-26 18:57:47 +0800

    fix plugin

--- side/omega_python_plugins/关键词触发.py
+++ side/omega_python_plugins/关键词触发.py
@@ -9,7 +9,14 @@ def keywords_plugin(api:API):
     # 发送一条消息到 omega 并接收，可以用来测试连接
     response=api.do_echo(">>>关键词plugin连接成功<<<",cb=None)
     print(response.msg)
-    response=api.listen_mc_packet(pkt_type="IDText",cb=None,on_new_packet_cb=on_text_packet)
+    def on_text_packet(packet):
+        print("接受到了聊天数据包") # 每种包内容都不一样，具体内容自己看
+        print("数据包内容为: ",packet) #{'TextType': 1, 'NeedsTranslation': False, 'SourceName': '2401PT', 'Message': '啊吧', 'Parameters': None, 'XUID': '', 'PlatformChatID': '', 'PlayerRuntimeID': ''}
+    
+    # 订阅某类数据包，比如这个就是聊天的数据包
+    response=api.listen_mc_packet(pkt_type="IDText",cb=None,on_new_packet_cb=on_text_packet) # 有哪些数据包请查看开发文档
+    print(response.succ) # True
+    print(response.err) # None
 
 
 omega.add_plugin(plugin=keywords_plugin)
