cab813fa31f4079b8c53643721059d38e72978f6
Yomiku0617 <110821146+Yomiku0617@users.noreply.github.com>
2022-08-26 22:18:43 +0800

    fix 0.6

--- side/omega_python_plugins/关键词触发.py
+++ side/omega_python_plugins/关键词触发.py
@@ -1,5 +1,13 @@
 # 插件: 开
-
+#install_lib(lib_name="logging",lib_install_name="logging")
+import logging
+logging.basicConfig(
+    level=logging.DEBUG, # 由于默认日志级别为WARNING， 只打印了WARNING, ERROR, CRITICAL的日志
+    format='\033[46;30m %(levelname)s \033[0m\033[36m %(message)s \033[0m',
+    datefmt='%H:%M:%S',
+    # 'filename': 'test.log',
+    # 'filemode': 'w' # 默认是追加模式
+)
 import time
 from omega_side.python3_omega_sync import API
 from omega_side.python3_omega_sync import frame as omega
@@ -9,13 +17,13 @@ from omega_side.python3_omega_sync.protocol import *
 def keywords_plugin(api:API):
     # 发送一条消息到 omega 并接收，可以用来测试连接
     response=api.do_echo("关键词插件启动成功",cb=None)
-    print(response.msg) # hello
+    print(str(response.msg)) # hello
     response=api.do_get_players_list(cb=None)
     for player in response:
-        print("当前玩家名："+player.name) #OmeGoTest
-        print(player.runtimeID) #0
-        print(player.uuid) #00000000-0000-4000-8000-0000392af26c
-        print(player.uniqueID) #-214748364274
+        logging.info("当前玩家名："+str(player.name))
+        print("\033[34m当前玩家名:"+str(player.name)+"\033[0m")
+    response=api.do_send_ws_cmd("/say 执行成功！",cb=None)
+    logging.info(response.result.CommandOrigin)
 
 
 omega.add_plugin(plugin=keywords_plugin)
